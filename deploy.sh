644616f293a45264b2a74ffcfdc7e9d7a0cbc9a4
Filipe Oliveira <contato@fmoliveira.com.br>
2016-12-16 01:19:10 -0200

    Mounting volume to store built sites.

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ docker run \
   --restart=always \
   --link mob-db-mongo:db \
   --link mob-mq-rabbit:mq \
+  -v /home/fmob/repos/customers:/var/customers \
   -e MOB_MONGO_FACEBOOK_DATABASE='mongodb://db:27017/mobyourlife_facebook' \
   -e MOB_RABBITMQ_URL='amqp://mq' \
   -d \
