94c355f9210f426f9a35f55573f0f7d09c8e0ebe
Filipe Oliveira <contato@fmoliveira.com.br>
2016-12-16 01:03:38 -0200

    Setting log fields properly after the build.

--- src/objects/set-build-time.js
+++ src/objects/set-build-time.js
@@ -3,7 +3,10 @@ export function setBuildTime(db, fb_account_id) {
     try {
       const pages = db.collection('pages')
       pages.update({ fb_account_id }, {
-        $set: { log: { last_built: new Date() } }
+        $set: {
+          'log.last_built': new Date(),
+          'log.build_updated': true
+        }
       }, (err, result) => {
         if (err) {
           reject('Unable to set build time!' + err)
